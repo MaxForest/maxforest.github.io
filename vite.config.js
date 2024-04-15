86f7b6f243fd3a3e8b6fae9cc5225b2ea8792fb1
Maxime Aubin-Forest <maxime.af@outlook.com>
2024-04-15 13:39:25 -0400

    vite and vue

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,13 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/maxforest.github.io/",
   plugins: [
     vue(),
     VueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
